ae87965d733aa49606703536c3336a0352c41af4
patgarcia <commonzenmedia@gmail.com>
2022-02-03 22:56:01 -0500

    Add custom hook logic

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# useFormReducer
+# [useFormReducer](https://reactjs.org/) &nbsp; ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+`useFormReducer` is a custom hook that provides the ability to have controlled-forms without the hassle. It implements a reducer via react's `useReducer` hook.
+
+It abstracts several patterns involving controlled-forms:
+
+- **handle change:** pass a "state setter" to your form input `onChange` attribute and it updates the state without going through destructuring the event key + value with the state.
+- **validation:** validate an email field by default or pass custom validators to provide support for different field-type validation.
+- **error state:** pass an error state setter to capture validation errors.
+
+## use
+
+```bash
+$ npm install use-form-reducer
+```
+
+## use
+
+Within the component that holds the form:
+
+```jsx
+// import the package
+import useFormReducer from 'use-form-reducer';
+
+// set the form state hook
+const [form, setForm] = useFormReducer();
+
+// within the form element
+<form>
+  ...
+  <input
+    type="email"
+    value={form.email}
+    name="email"
+    onChange={setForm}
+    onBlur={setForm}
+  />
+  ...
+</form>;
+```
+
+\* Setting `onBlur` provides specific validation when leaving the input field. An example of this logic is available within the included validator for reference. When using a validator, a passing the `errorSetter` argument is recommended.
+
+## More info
+
+- This may be obvioys but just-in-case, you can use the `form` state object to pass it in the body of your request.
+
+## Special thanks:
+
+Shoutout to [@stevekinney](https://github.com/stevekinney) for sharing the cool trick of using reducers to simplify state management in a form, and avoid the use of the `setState` hook. [State Management in Pure React, v2](https://frontendmasters.com/courses/pure-react-state/)
+
+### License
+
+React is [MIT licensed](./LICENSE).
---
+++ index.js
@@ -0,0 +1,78 @@
+import { useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+const useFormReducer = (
+  errorSetter,
+  customInitForm,
+  customValidator,
+  customReducer,
+  actionDispatcher,
+  actionCreators
+) => {
+  const initForm = customInitForm || {
+    email: '',
+    password: '',
+  };
+
+  const formReducer =
+    customReducer ||
+    ((form, formInput) => {
+      return { ...form, ...formInput };
+    });
+
+  const [state, dispatch] = useReducer(formReducer, initForm);
+
+  /***************************************************************************************
+   *    Title: validateEmail
+   *    Author: Zaheer Paracha
+   *    Date: 2008
+   *    Code version: 1.0
+   *    Availability: http://zparacha.com/validate-email-address-using-javascript-regular-expression
+   ***************************************************************************************/
+  function validateEmail(elementValue) {
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue);
+  }
+
+  const formValidation =
+    // TODO: Have the validation use an app level dispatcher to decouple custom error setting
+    customValidator ||
+    (!!errorSetter
+      ? (ev, errorSetter, actionDispatcher, actionCreators) => {
+          if (
+            ev.type === 'blur' &&
+            ev.target.name === 'email' &&
+            !validateEmail(state.email)
+          ) {
+            const errObj = ['email', 'must be a valid format'];
+            errorSetter(err =>
+              !!err ? [...err.filter(e => e[0] !== 'email'), errObj] : [errObj]
+            );
+          } else {
+            errorSetter(err =>
+              !!err ? err.filter(e => e[0] !== 'email') : null
+            );
+          }
+        }
+      : () => {});
+
+  // TODO: Refactor formValidation to partial to require just ev
+  const customDispatch = ev => {
+    formValidation(ev, errorSetter, actionDispatcher, actionCreators);
+    const { name, value } = ev.target;
+    dispatch({ [name]: value });
+  };
+
+  return [state, customDispatch];
+};
+
+useFormReducer.propTypes = {
+  errorSetter: PropTypes.func,
+  customInitForm: PropTypes.objectOf(PropTypes.shape()),
+  customValidator: PropTypes.func,
+  customReducer: PropTypes.func,
+  actionDispatcher: PropTypes.func,
+  actionCreators: PropTypes.objectOf(PropTypes.shape()),
+};
+
+module.exports = useFormReducer;
